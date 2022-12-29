b66ae649c4daac414629a590918b59c27b061559
Adriana Cucu <adrianacucu99@gmail.com>
2022-12-29 22:01:37 +0000

    Disabled chatGPT until fronted is usable

--- cv-assistant-server/src/chatgptClient.ts
+++ cv-assistant-server/src/chatgptClient.ts
@@ -1,24 +1,29 @@
-import {ChatGPTAPIBrowser} from 'chatgpt';
+import { ChatGPTAPIBrowser } from 'chatgpt';
+
+let chatgptApi: ChatGPTAPIBrowser;
+
+export async function init(): Promise<void> {
+    // const email: string | undefined = process.env.OPENAI_EMAIL;
+    // const password: string | undefined = process.env.OPENAI_PASSWORD;
+
+    // if (!email || !password) {
+    //     throw new Error("Missing ChatGPT credentials.");
+    // }
+
+    // // use puppeteer to bypass cloudflare (headful because of captchas)
+    // const api = new ChatGPTAPIBrowser({
+    //     email,
+    //     password,
+    //     // markdown: false
+    // });
+
+    // // TODO: figure this out
+    // await api.initSession();
+    // chatgptApi = api;
+}
 
 export async function sendMessage(): Promise<string> {
-    const email: string | undefined = process.env.OPENAI_EMAIL;
-    const password: string | undefined = process.env.OPENAI_PASSWORD;
-
-    if (!email || !password) {
-        throw new Error("Missing ChatGPT credentials.");
-    }
-
-    // use puppeteer to bypass cloudflare (headful because of captchas)
-    const api = new ChatGPTAPIBrowser({
-        email,
-        password,
-        // markdown: false
-    });
-
-    // await api.initSession();
-
-    // const result = await api.sendMessage('Hello World!');
+    // const result = await chatgptApi.sendMessage('Hello World!');
     // return result.response;
-
-    return "banana";
+    return "ChatGPT disbled.";
 }
--- cv-assistant-server/src/index.ts
+++ cv-assistant-server/src/index.ts
@@ -2,13 +2,16 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
-import { sendMessage } from "./chatgptClient";
+import { init, sendMessage } from "./chatgptClient";
 
 dotenv.config();
 
 const app: Express = express();
 app.use(cors()); // TODO: configure for specific origin
 const port = process.env.PORT || 8080;
+
+// Setup ChatGPT session
+await init();
 
 // TODO: endpoint for ChatGPT interaction
 // TODO: try to just call the function without catching
@@ -21,13 +24,13 @@ app.get("/", (req: Request, res: Response) => {
 app.get("/banana", async (req: Request, res: Response, next) => {
     try {
         const result = await sendMessage();
-        res.send({ message: result });
+        res.status(200).send({ message: result });
     } catch (err) {
         const error = err as Error;
-        res.status(500).send({ message: error.message});
+        res.status(500).send({ message: error.message });
     }
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`Server running on localhost:${port}`);
 });
